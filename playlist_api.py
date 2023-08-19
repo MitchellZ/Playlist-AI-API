617606bcc4055ce1b66a647b35ae70c15298fdd8
MitchellZ <mitchellzoph@gmail.com>
2023-08-19 18:24:59 -0500

    Add audio endpoint

--- playlist_api.py
+++ playlist_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, redirect, request
 from flask_cors import CORS
 
 # Import PaLM Service functions
@@ -28,7 +28,7 @@ def ping():
     }
     return jsonify(response), 500
 
-# Example Endpoint
+# Playlist generation endpoint
 @app.route('/query', methods=['GET'])
 
 def query():
@@ -64,6 +64,29 @@ def query():
             }
             return jsonify(response), 500
 
+# Audio endpoint
+@app.route('/audio', methods=['GET'])
+def get_audio():
+    video_id = request.args.get('id')  # Retrieve the 'id' parameter from the query string
+
+    if video_id is None:
+        response = {
+            'message': 'No video ID provided!'
+        }
+        return jsonify(response), 400
+
+    audio_url = f"https://vid.puffyan.us/latest_version?id={video_id}&itag=140"
+
+    try:
+        return redirect(audio_url)  # Redirect the user to the audio URL
+    except Exception as e:
+        # Error
+        print('Error:', e)
+        response = {
+            'message': 'Error redirecting to audio URL!'
+        }
+        return jsonify(response), 500
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=5000, debug=True)
